56124f25ba3a1628213d3393a126107f8bab42d8
ch <mkmsacc@outlook.com>
2024-08-24 21:27:09 +0700

    Add HelloController with greeting endpoint
    Introduced HelloController to handle root URL requests and return a "hello" message. Updated WebApplicationTests to include a test verifying the new endpoint functionality.

--- src/test/java/io/github/chubbyhippo/web/WebApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/web/WebApplicationTests.java
@@ -1,13 +1,31 @@
 package io.github.chubbyhippo.web;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WebApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("should return hello")
+	void shouldReturnHello() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+						.string("hello"));
 	}
 
 }
